0f05ebf9d3fb9f0482e718245414da4c26fd07a1
502971@qq.com <yxw770@gmail.com>
2021-03-29 03:14:07 +0800

    1

--- cs.py
+++ cs.py
@@ -11,8 +11,9 @@ def find(jd, one):
         while json_data['code'] == 0:
             time.sleep(random.randint(5, 10))
             json_data = json.loads(requests.get("http://query.shuzhikj.com/api/platform/get_taskrank?id=" + jd['data'], headers=headers).text)
+            print('执行查询》》', '关键词：', one[3], json_data)
         else:
-            print("[查询成功]任务排名为:", json_data['data'], "原始数据为:", json_data)
+            print('查询成功》》', '关键词为：', one[3], '排名:', json_data['data'], "原始数据为:", json_data)
             config = {
                 'host': 'localhost',
                 'port': 3306,
@@ -22,7 +23,7 @@ def find(jd, one):
             }
             con = mysql.connector.connect(**config)
             cursor = con.cursor()
-            cursor.execute('update fa_anchor set status=2, rank=' + str(json_data['data']) + ' where id=' + str(one[0]))
+            cursor.execute('update task set status=2, rank=' + str(json_data['data']) + ' where id=' + str(one[0]))
             print('传输成功:', requests.post(url="https://t.idccap.com/api/v1/QuickRanking/updata_rank", data={'id': one[1], 'rank': json_data['data']}).text)
 # 服务器信息
 config = {
@@ -34,7 +35,7 @@ config = {
 }
 con = mysql.connector.connect(**config)
 cursor = con.cursor()
-cursor.execute('select * from fa_anchor where status = 0 or status = 3')
+cursor.execute('select * from task where status = 0 or status = 3')
 taskList = cursor.fetchall()
 headers = {
     'apikey': 'mJDNNXnsu946MLB'
@@ -43,6 +44,6 @@ for one in taskList:
     url = 'http://query.shuzhikj.com/api/platform/add_task?name=' + str(one[3]) + '&url=' + str(one[2])+'&type=1&urlmode=1'
     json_data = json.loads(requests.get(url=url, headers=headers).text)
     print('任务 >> ', '关键词：', one[3], json_data)
-    for _ in range(5):
+    for _ in range(1):
         threading.Thread(target=find, args=(json_data, one)).start()
 con.close()
